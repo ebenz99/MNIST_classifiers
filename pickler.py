6c73fd8b1f1d28e7065247b136e282776a0af399
unknown <ebenz99@gmail.com>
2018-05-23 13:11:18 -0400

    now brings in test labels

--- pickler.py
+++ pickler.py
@@ -1,11 +1,8 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 # Imports
 import numpy as np
 import tensorflow as tf
 import pickle
+import matplotlib.pyplot as plt
 
 
 def main(args):
@@ -14,6 +11,7 @@ def main(args):
   train_data = mnist.train.images # Returns np.array
   train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
   eval_data = mnist.test.images # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
   np.save("trainImages", train_data, allow_pickle=True)
   np.save("trainLabels", train_labels, allow_pickle=True)
